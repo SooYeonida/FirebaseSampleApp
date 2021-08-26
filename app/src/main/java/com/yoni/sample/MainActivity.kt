373d7009502e199040ba361a2dac13bdfa600e7e
sooyeon <qkrtndus130@gmail.com>
2021-08-27 02:57:36 +0900

    dataBinding 연습

--- app/src/main/java/com/yoni/sample/MainActivity.kt
+++ app/src/main/java/com/yoni/sample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -16,23 +17,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
 
         // Initialize Firebase Auth
         auth = Firebase.auth
 
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        val email = binding.editEmail.text.toString()
+        val pwd = binding.editPwd.text.toString()
+
         binding.btnJoin.setOnClickListener {
-            auth.createUserWithEmailAndPassword("email@test.com", "pwd test")
+            auth.createUserWithEmailAndPassword(email, pwd)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         Toast.makeText(this,"join success",Toast.LENGTH_SHORT).show()
                     } else {
+                        println(task.result)
                         Toast.makeText(this,"join fail",Toast.LENGTH_SHORT).show()
                     }
                 }
         }
+
     }
 
 }
